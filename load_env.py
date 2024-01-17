0047e518041647e5cf01371685587bd60b045d47
maxencelupion <maxence.lupion@epitech.eu>
2024-01-17 16:27:24 +0100

    [FIX] load_env.py file fixed

--- load_env.py
+++ load_env.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 TOKEN: str = os.getenv('DISCORD_TOKEN')
 LOL_API_KEY: str = os.getenv("LOL_API_KEY")
+GUILD_ID: str = os.getenv("GUILD_ID")
 HOST_DB: str = os.getenv("HOST_DB")
 USER_DB: str = os.getenv("USER_DB")
 PASSWORD_DB: str = os.getenv("PASSWORD_DB")
