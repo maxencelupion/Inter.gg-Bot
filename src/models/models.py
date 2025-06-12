726982447e231b9b384a862d0f733cdece101161
maxencelupion <maxencelupion@gmail.com>
2025-06-13 00:26:39 +0200

    [ADD] models.py uptdated -> need to add a migration library

--- src/models/models.py
+++ src/models/models.py
@@ -10,9 +10,8 @@ class Server(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     server_id: Mapped[int] = mapped_column(unique=True)
     channel_id: Mapped[int]
-    created_at = mapped_column(DateTime, default=datetime.utcnow)
-    updated_at = mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
+    created_at = mapped_column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))
+    updated_at = mapped_column(DateTime, default=datetime.datetime.now(datetime.timezone.utc), onupdate=datetime.datetime.now(datetime.timezone.utc))
     accounts: Mapped[list["Account"]] = relationship(back_populates="server", cascade="all, delete-orphan")
 
 class Account(Base):
@@ -22,7 +21,8 @@ class Account(Base):
     user_tag: Mapped[str]
     encrypted_id: Mapped[str]
     discord_user: Mapped[str]
-    server_id: Mapped[int] = mapped_column(ForeignKey("server.id"), nullable=False)
+    server_id: Mapped[int] = mapped_column(ForeignKey("server.id"))
+    server: Mapped["Server"] = relationship(back_populates="accounts")
     in_game: Mapped[bool] = mapped_column(default=False, nullable=False)
     division_solo: Mapped[str] = mapped_column(default='Unranked', nullable=False)
     tier_solo: Mapped[str] = mapped_column(default='IV', nullable=False)
@@ -30,7 +30,5 @@ class Account(Base):
     division_flex: Mapped[str] = mapped_column(default='Unranked', nullable=False)
     tier_flex: Mapped[str] = mapped_column(default='IV', nullable=False)
     league_points_flex: Mapped[int] = mapped_column(default=0, nullable=False)
-    created_at = mapped_column(DateTime, default=datetime.utcnow)
-    updated_at = mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow) # Used to track inactive accounts
-
-server: Mapped["Server"] = relationship(back_populates="accounts")
+    created_at = mapped_column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))
+    updated_at = mapped_column(DateTime, default=datetime.datetime.now(datetime.timezone.utc), onupdate=datetime.datetime.now(datetime.timezone.utc)) # Used to track inactive accounts
